cae5fe6d95c295033f116db89f353c434d633068
Khagastra <khagastra@gmail.com>
2025-05-18 15:56:11 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astrofy-template.netlify.app',
+  base: '/khagastra.github.io/',
   integrations: [mdx(), sitemap(), tailwind()]
 });
